779011f12406e373843e91d75710017131f00e82
talentafigan <talentafigan@gmail.com>
2022-11-23 02:19:16 +0700

    fix : setting cors

--- app.js
+++ app.js
@@ -7,21 +7,17 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 var indexRouter = require("./controllers/index");
 const ControllerUser = require("./controllers/user");
-var cors = require('cors')
 const ControllerAuth = require("./controllers/auth");
 var app = express();
 
-app.use(cors)
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
-
 app.use(express.static("public"));
 
 app.use(logger("dev"));
